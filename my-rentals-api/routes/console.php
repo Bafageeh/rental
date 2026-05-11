9df615ba0e1170eb7744314a0477b1fca3f114e9
Bafageeh <a.baf@live.com>
2026-05-11 18:40:11 +0300

    Schedule daily rent overdue WhatsApp report

--- my-rentals-api/routes/console.php
+++ my-rentals-api/routes/console.php
@@ -2,7 +2,14 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Schedule::command('rent:send-overdue-whatsapp-report')
+    ->dailyAt('18:25')
+    ->timezone('Asia/Riyadh')
+    ->withoutOverlapping()
+    ->onOneServer();
