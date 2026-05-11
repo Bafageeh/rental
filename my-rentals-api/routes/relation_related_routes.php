ac9dab383406d68557421481d6aa295805c6f43e
Bafageeh <a.baf@live.com>
2026-05-11 09:06:12 +0300

    Use official payment dates relation override first

--- my-rentals-api/routes/relation_related_routes.php
+++ my-rentals-api/routes/relation_related_routes.php
@@ -1,9 +1,15 @@
 <?php
 
 // تفاصيل السجلات والعلاقات التابعة.
-// نستخدم override خاص بالعقود حتى تظهر التبويبة باسم الدفعات وتُراجع تواريخ الدفعات عند فتح العقد.
+// نحمّل override الخاص بتواريخ دفعات PDF الرسمية قبل أي override قديم لأن Laravel يعتمد أول مسار مطابق.
 
 require_once base_path('app/Support/RelationRelatedHelpers.php');
+
+$officialOverride = base_path('routes/api/000_contract_details_official_payment_dates.php');
+if (is_file($officialOverride)) {
+    require $officialOverride;
+    return;
+}
 
 $override = base_path('routes/api/107_contract_details_payments_override.php');
 if (is_file($override)) {
