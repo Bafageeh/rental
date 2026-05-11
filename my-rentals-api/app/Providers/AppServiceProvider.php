0e1ac8453029ed4ef9b2c81a35442ff516ed5e7a
Bafageeh <a.baf@live.com>
2026-05-11 08:06:21 +0300

    Use enhanced Ejar PDF extractor

--- my-rentals-api/app/Providers/AppServiceProvider.php
+++ my-rentals-api/app/Providers/AppServiceProvider.php
@@ -4,6 +4,8 @@ namespace App\Providers;
 
 use App\Models\WebhookEvent;
 use App\Observers\WebhookEventObserver;
+use App\Services\EnhancedGovernmentContractPdfExtractor;
+use App\Services\GovernmentContractPdfExtractor;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,7 +15,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->bind(GovernmentContractPdfExtractor::class, EnhancedGovernmentContractPdfExtractor::class);
     }
 
     /**
