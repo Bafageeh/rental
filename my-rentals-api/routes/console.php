8ec5b50e6f70a287a210073bdb1ebcf38ee0a873
Bafageeh <a.baf@live.com>
2026-05-31 22:05:55 +0200

    Use compact table command for overdue scheduled report

--- my-rentals-api/routes/console.php
+++ my-rentals-api/routes/console.php
@@ -4,6 +4,8 @@ use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Schedule;
 
+require_once __DIR__ . '/console_overdue_table_report.php';
+
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
