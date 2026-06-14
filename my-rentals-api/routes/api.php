f91e85c6cccf9445a26dfccb17ba9bbf331c13d8
Bafageeh <a.baf@live.com>
2026-06-14 14:09:47 +0300

    Load account privacy request route

--- my-rentals-api/routes/api.php
+++ my-rentals-api/routes/api.php
@@ -138,6 +138,7 @@ Route::middleware(['auth.api'])->group(function () {
         __DIR__ . '/api/127_chat_ticket_close.php',
         __DIR__ . '/api/128_tenant_reports.php',
         __DIR__ . '/api/132_privacy_requests.php',
+        __DIR__ . '/api/133_account_requests.php',
     ] as $publicAuthenticatedRouteModule) {
         if (is_file($publicAuthenticatedRouteModule)) require $publicAuthenticatedRouteModule;
     }
