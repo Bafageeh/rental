53e6625fd0d1d2ac8ea1f4b7da66d3c8313c466a
Bafageeh <a.baf@live.com>
2026-09-01 00:26:56 +0300

    Replace React Navigation focus hook with Expo Router

--- my-rentals-mobile/src/app/owner/[id].tsx
+++ my-rentals-mobile/src/app/owner/[id].tsx
@@ -1,19 +1,17 @@
-import { useEffect, useState } from "react";
-import { useLocalSearchParams } from "expo-router";
-import { useIsFocused } from "@react-navigation/native";
+import { useCallback, useState } from "react";
+import { useFocusEffect, useLocalSearchParams } from "expo-router";
 import OwnerAssetsDashboardScreen from "../../components/OwnerAssetsDashboardCompactScreen";
 
 export default function OwnerDashboardRoute() {
   const params = useLocalSearchParams<{ id: string }>();
   const id = String(params.id || "");
-  const isFocused = useIsFocused();
   const [refreshKey, setRefreshKey] = useState(0);
 
-  useEffect(() => {
-    if (isFocused) {
+  useFocusEffect(
+    useCallback(() => {
       setRefreshKey((value) => value + 1);
-    }
-  }, [isFocused]);
+    }, [id])
+  );
 
   return <OwnerAssetsDashboardScreen key={`${id}-${refreshKey}`} id={id} />;
 }
